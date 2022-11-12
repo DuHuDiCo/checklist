9ad2db318002be3b67ed7bf97eaf574386697c2b
Duvan Diaz <dudicodiaz@gmail.com>
2022-11-12 15:47:06 -0500

    c

--- src/main/java/com/checklist/checklist/ChecklistApplication.java
+++ src/main/java/com/checklist/checklist/ChecklistApplication.java
@@ -2,18 +2,18 @@ package com.checklist.checklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-////import org.springframework.boot.builder.SpringApplicationBuilder;
-////import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChecklistApplication {
+public class ChecklistApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(ChecklistApplication.class, args);
     }
-//    
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(ChecklistApplication.class);
-//    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChecklistApplication.class);
+    }
 }
